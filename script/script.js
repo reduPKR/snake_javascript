acfffabc47c38df8e4ab6bae1d51e71412fd8553
redu <reduardo19942@gmail.com>
2021-01-16 16:24:26 -0300

    Condiçao de derrota

--- script/script.js
+++ script/script.js
@@ -50,6 +50,12 @@ function iniciarJogo(){
     if(snake[0].y < 0 && direction == 'up') 
         snake[0].y = 16 * box;
 
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+    }
 
     criarBG();
     criarCobrinha();
